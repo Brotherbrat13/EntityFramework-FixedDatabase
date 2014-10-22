4a5089f01ad1d0d5da10afee04acc02cd7f7f996
Brotherbrat13 <brotherbrat13@gmail.com>
2014-10-22 14:36:25 -0400

    Project Added
    Good luck have fun

---
+++ AttemptNumberThree/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AttemptNumberThree.Startup))]
+namespace AttemptNumberThree
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
