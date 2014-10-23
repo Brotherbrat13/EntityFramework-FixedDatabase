3a6108354ce1fc5ac24729c10f51d3e948f3fd5c
csz1 <csz1@dowling.edu>
2014-10-23 10:07:24 -0400

    Field part 1
    added basic code for username field.. need to add actual field

--- AttemptNumberThree/Models/AccountViewModels.cs
+++ AttemptNumberThree/Models/AccountViewModels.cs
@@ -79,6 +79,11 @@ namespace AttemptNumberThree.Models
         [Display(Name = "Confirm password")]
         [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
         public string ConfirmPassword { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 6)]
+        [Display(Name = "Username")]
+        public string Username { get; set; }
     }
 
     public class ResetPasswordViewModel
